face5f41a812bd11f2a290ff0b7aa72b84fec5b7
bondanjody <bondanjodys@gmail.com>
2023-03-23 18:54:57 +0700

    Menambahkan feedback ketika loading pengiriman data order, dan ketika sudah berhasil terkirim. Lalu menambahkan method clearCart untuk membersihkan cart ketika kita sudah selesai mengirim data.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { Fragment, useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
@@ -8,6 +8,9 @@ import Checkout from './Checkout';
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -25,14 +28,18 @@ const Cart = (props) => {
     setIsCheckout(true);
   }
 
-  const submitOrderHandler = userData => {
-    fetch('https://react-http-request-ab44d-default-rtdb.firebaseio.com/orders.json', {
+  const submitOrderHandler = async userData => {
+    setIsSubmitting(true);
+    await fetch('https://react-http-request-ab44d-default-rtdb.firebaseio.com/orders.json', {
       method: 'POST',
       body: JSON.stringify({
         user: userData,
         orderedItems: cartCtx.items
       })
     });
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   }
 
   const cartItems = (
@@ -57,15 +64,35 @@ const Cart = (props) => {
       {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
   </div>;
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
+  // Tampilan ketika belum mengirim data order
+  const cartModalContent = <Fragment>
+    {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
       {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
       {!isCheckout && modalAction}
+  </Fragment>;
+
+  // Tampilan ketika loading mengirim data order
+  const isSubmittingModalContent = <p>Sending order data ...</p>;
+
+  // Tampilan ketika berhasil mengirim data order
+  const didSubmitModalContent = <Fragment>
+    <p>Successfully sent the order !</p>
+    <div className={classes.actions}>
+      <button className={classes.button} onClick={props.onClose}>
+        Close
+      </button>
+    </div>
+    </Fragment>;
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -33,6 +33,7 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
+
   }
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
@@ -55,6 +56,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -72,11 +77,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({type: 'CLEAR'});
+  }
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
